21b5ca47b0e4b329d8a575ace780a52c7ee27e4f
AtalibAgAlmousleck <atalibletouareg@gmail.com>
2024-11-20 01:11:49 +0800

    Java mail send service

---
+++ backend/src/main/java/com/almousleck/service/EmailService.java
@@ -0,0 +1,30 @@
+package com.almousleck.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import java.io.UnsupportedEncodingException;
+
+@Service
+public class EmailService {
+    private final JavaMailSender javaMailSender;
+
+    public EmailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    public void sendEmail(String email, String subject, String content) throws MessagingException, UnsupportedEncodingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+
+        helper.setFrom("non-reply@almousleck.google.com", "LinkedIn Java Clone");
+        helper.setText(email);
+        helper.setSubject(subject);
+        helper.setText(content);
+
+        javaMailSender.send(message);
+    }
+}
